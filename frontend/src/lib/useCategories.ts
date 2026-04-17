22546d8bc6a498997c2d6c23d66febf3b58aabc5
Fabian <cto@sektbirne.fun>
2026-04-17 12:09:05 +0200

    [frontend] Add useCategoryCounts() React Query hook

--- frontend/src/lib/useCategories.ts
+++ frontend/src/lib/useCategories.ts
@@ -1,10 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
-import { getCategories } from './api'
+import { getCategories, getCategoryCounts } from './api'
 
 export function useCategories() {
   return useQuery({
     queryKey: ['categories'],
     queryFn: getCategories,
     staleTime: 1000 * 60 * 60, // 1 Stunde
+  })
+}
+
+export function useCategoryCounts() {
+  return useQuery({
+    queryKey: ['categoryCounts'],
+    queryFn: getCategoryCounts,
+    staleTime: 1000 * 60 * 60,
   })
 }
