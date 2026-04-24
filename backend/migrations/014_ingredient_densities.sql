3d12abb2d82a81f7b1fd433633577f50e078b06d
Fabian <cto@sektbirne.fun>
2026-04-24 13:38:48 +0200

    fix: remove trailing comma in granules array in migration 014
    [backend]

--- backend/migrations/014_ingredient_densities.sql
+++ backend/migrations/014_ingredient_densities.sql
@@ -112,7 +112,7 @@ INSERT INTO ingredient_density_keywords (id, type_id, keyword)
 SELECT gen_random_uuid(), id, k FROM ingredient_density_types, unnest(ARRAY[
   'granules','chilli granules','chilli flakes','dried chillies','chilli crisp',
   'red pepper flakes','paprika granules','getrocknete chilis','chiliflöckchen',
-  'dried chile','chili powder','paprika powder', 'flakes','chili flakes','chili granules','chili crisp','chili knusper','paprikaflocken',
+  'dried chile','chili powder','paprika powder', 'flakes','chili flakes','chili granules','chili crisp','chili knusper','paprikaflocken'
 ]) AS k WHERE type_name = 'granules'
 ON CONFLICT (keyword) DO NOTHING;
 
